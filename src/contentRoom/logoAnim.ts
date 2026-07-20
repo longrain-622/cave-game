ac9e310b09f1625c0401b43ea06596835066dbdc
LongRain <3685585221@qq.com>
2026-07-20 17:45:24 +0800

    update tsc and goto 1.0.0-alpha.1

--- src/contentRoom/logoAnim.ts
+++ src/contentRoom/logoAnim.ts
@@ -1,13 +1,18 @@
 import { apioxEvent } from "../apiox/event.js";
+import { apioxTime } from "../apiox/time.js";
 import { ApioxObject } from "../apiox/dom.js";
 import { win } from "../apiox/global.js";
 
-apioxEvent.listenGlobalOnce('DOMContentLoaded', () => {
+apioxEvent.listenGlobalOnce('DOMContentLoaded', (): void => {
     const logo = new ApioxObject('logo');
-    if (win.session.get('logoShown')) {
-        logo.domstyle('display', 'none');
+    if(win.session.get('logoShown') === 'true') {
+        logo.hide();
     } else {
-        win.session.set('logoShown', 'true');
-        logo.addClass('animate');
+        logo.show('flex');
+        apioxTime.setOut((): void => {
+            logo.hide();
+            //logo.addClass('animate');
+            win.session.set('logoShown', 'true');
+        }, 1000);
     }
 });
