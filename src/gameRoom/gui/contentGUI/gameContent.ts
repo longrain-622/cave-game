915ae2c982e923cb9c8c578e132e36721dff7e46
LongRain <3685585221@qq.com>
2026-08-03 23:55:12 +0800

    修复游戏菜单 bug

--- src/gameRoom/gui/contentGUI/gameContent.ts
+++ src/gameRoom/gui/contentGUI/gameContent.ts
@@ -7,16 +7,16 @@ import { lang } from "../../../others/i18n.js";
 import { guiApp } from "../application.js";
 import { room } from "../../../constants/generic.js";
 import { buttonTextures } from "../application.js";
-import { genericTextStyle } from "../../rendering.js";
+import { textStyle2 } from "../../../constants/pixiStyles.js";
 import { uistate } from "../uiState.js";
 import * as PIXI from 'pixi.js';
 
-let gameContent = new PIXI.Container();
+const gameContent = new PIXI.Container();
 gameContent.zIndex = 10;
 gameContent.visible = uistate.gameContent_isOpening;
 guiApp.stage.addChild(gameContent);
 
-//黑色背景
+// 黑色背景
 const blackBg = new PIXI.Graphics();
 blackBg.beginFill(0x000000, 0.7);
 blackBg.drawRect(0, 0, room.width, room.height);
@@ -25,7 +25,7 @@ blackBg.visible = true;
 gameContent.addChild(blackBg);
 gameContent.setChildIndex(blackBg, 0);
 
-//按钮尺寸
+// 按钮尺寸
 //const btnW = 200;
 const btnH = 20;
 const centerX = room.width / 2;
@@ -35,28 +35,28 @@ const btnScale = 3;
 
 // 创建按钮
 const topBtn = new PIXI.Sprite(buttonTextures.normal!);
-topBtn.anchor.set(0.5); //以中心点为基准定位
+topBtn.anchor.set(0.5); // 以中心点为基准定位
 topBtn.position.set(centerX, centerY - btnH / 2 - gap);
 topBtn.eventMode = 'static';
 topBtn.cursor = 'pointer';
 topBtn.scale.set(btnScale, btnScale);
-topBtn.on('pointerover', () => { //鼠标/指针进入
+topBtn.on('pointerover', () => { // 鼠标/指针进入
     topBtn.texture = buttonTextures.hover;
 });
-topBtn.on('pointerout', () => { //鼠标/指针离开
+topBtn.on('pointerout', () => { // 鼠标/指针离开
     topBtn.texture = buttonTextures.normal;
 });
 
-const bottomBtn = new PIXI.Sprite(buttonTextures.normal!); //第二个按钮
+const bottomBtn = new PIXI.Sprite(buttonTextures.normal!); // 第二个按钮
 bottomBtn.anchor.set(0.5);
 bottomBtn.position.set(centerX, centerY + btnH / 2 + gap);
 bottomBtn.eventMode = 'static';
 bottomBtn.cursor = 'pointer';
 bottomBtn.scale.set(btnScale, btnScale);
-bottomBtn.on('pointerover', () => { //鼠标/指针进入
+bottomBtn.on('pointerover', () => { // 鼠标/指针进入
     bottomBtn.texture = buttonTextures.hover;
 });
-bottomBtn.on('pointerout', () => { //鼠标/指针离开
+bottomBtn.on('pointerout', () => { // 鼠标/指针离开
     bottomBtn.texture = buttonTextures.normal;
 });
 
@@ -64,28 +64,41 @@ gameContent.addChild(topBtn, bottomBtn);
 topBtn.visible = true;
 bottomBtn.visible = true;
 
-const txt = { //文本对象
-    backToGame: new PIXI.Text('', genericTextStyle()),
-    backToTitle: new PIXI.Text('', genericTextStyle()),
-}
+const txt = { // 文本对象
+    title: new PIXI.Text('', textStyle2()),
+    backToGame: new PIXI.Text('', textStyle2()),
+    backToTitle: new PIXI.Text('', textStyle2()),
+};
 
 /*设置文本样式*/ {
     const fontSize: number = 24;
+    txt.title.style.fontSize = fontSize;
     txt.backToGame.style.fontSize = fontSize;
-    txt.backToGame.style.fontSize = fontSize;
+    txt.backToTitle.style.fontSize = fontSize;
+
+    txt.title.anchor.set(0.5);
     txt.backToGame.anchor.set(0.5);
     txt.backToTitle.anchor.set(0.5);
+
     const fontY_offset: number = 4;
-    txt.backToGame.position.set(topBtn.x, topBtn.y + fontY_offset); //位置与 topBtn 相同
+    txt.title.position.set(room.width / 2, room.height / 4);
+    txt.backToGame.position.set(topBtn.x, topBtn.y + fontY_offset); // 位置与 topBtn 相同
     txt.backToTitle.position.set(bottomBtn.x, bottomBtn.y + fontY_offset); //位置与 bottomBtn 相同
+}
+
+interface GameContentTexts {
+    title: string;
+    backToGame: string;
+    exitToContent: string;
 }
 
 async function loadGameContentTexts() {
     try {
-        const data = await apioxHttp.get<{ gameContent: { backToGame: string; exitToContent: string } }>(`/assets/locales/${lang}/game.json`);
-        const specificOrder = ['backToGame', 'exitToContent'] as const;
-        const [backText, exitText] = specificOrder.map(key => data.gameContent[key]);
+        const data = await apioxHttp.get<{ gameContent: GameContentTexts }>(`/assets/locales/${lang}/game.json`);
+        const specificOrder = ['title', 'backToGame', 'exitToContent'] as const;
+        const [titleText, backText, exitText] = specificOrder.map(key => data.gameContent[key]);
 
+        txt.title.text = titleText;
         txt.backToGame.text = backText;
         txt.backToTitle.text = exitText;
     } catch (error) {
@@ -94,6 +107,7 @@ async function loadGameContentTexts() {
 }
 loadGameContentTexts();
 
+gameContent.addChild(txt.title);
 gameContent.addChild(txt.backToGame);
 gameContent.addChild(txt.backToTitle);
 
